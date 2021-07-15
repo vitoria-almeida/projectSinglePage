daa263d5646a366729d1e4b1e9ddbee632cc0b98
Vitoria-m <vitoriazzz@outlook.com>
2021-07-15 13:03:48 -0300

    commit projeto single page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SinglePageComponent } from './single-page/single-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinglePageComponent
   ],
   imports: [
     BrowserModule
